6391a26cde0f50300d61b3464ac39326c8ed3331
yeonjeong <wjdwjdduswjd@naver.com>
2021-03-03 03:00:31 +0900

    후기 게시판 파일업로드

---
+++ RoundTheVillage/src/main/java/com/kh/RoundTheVillage/board/model/exception/UpdateAttachmentFailException.java
@@ -0,0 +1,14 @@
+package com.kh.RoundTheVillage.board.model.exception;
+
+public class UpdateAttachmentFailException extends RuntimeException {
+	// RuntimeException은 예외처리를 꼭 하지 않아도 되는
+	// unchecked Exception의 최상위 부모이다.
+	
+	// 이를 상속받은 자식은 모두 unchecked Exception이 된다.
+	
+	public UpdateAttachmentFailException(String message) {
+		super(message);
+	}  
+	
+	
+}
--- RoundTheVillage/src/main/java/com/kh/RoundTheVillage/board/model/vo/Attachment.java
+++ RoundTheVillage/src/main/java/com/kh/RoundTheVillage/board/model/vo/Attachment.java
@@ -1,17 +1,28 @@
 package com.kh.RoundTheVillage.board.model.vo;
 
+import java.sql.Timestamp;
+
 public class Attachment {
 
 	private int fileNo;
 	private String fileName;
-	private String fileLevel;
-	private int fileCreateDate;
-	private int filePath;
+	private int fileLevel;
+	/* private Timestamp fileCreateDate; */
+	private String filePath;
 	private int boardNo;
 	
-	
-	 public Attachment() {
+
+	 
+	public Attachment() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public Attachment(String filePath, String fileName, int fileLevel, int boardNo) {
+		super();
+		this.filePath = filePath;
+		this.fileName = fileName;
+		this.fileLevel = fileLevel;
+		this.boardNo = boardNo;
 	}
 
 
@@ -35,32 +46,30 @@ public class Attachment {
 	}
 
 
-	public String getFileLevel() {
+	public int getFileLevel() {
 		return fileLevel;
 	}
 
 
-	public void setFileLevel(String fileLevel) {
+	public void setFileLevel(int fileLevel) {
 		this.fileLevel = fileLevel;
 	}
 
 
-	public int getFileCreateDate() {
-		return fileCreateDate;
-	}
+	/*
+	 * public Timestamp getFileCreateDate() { return fileCreateDate; }
+	 * 
+	 * 
+	 * public void setFileCreateDate(Timestamp fileCreateDate) { this.fileCreateDate
+	 * = fileCreateDate; }
+	 */
 
-
-	public void setFileCreateDate(int fileCreateDate) {
-		this.fileCreateDate = fileCreateDate;
-	}
-
-
-	public int getFilePath() {
+	public String getFilePath() {
 		return filePath;
 	}
 
 
-	public void setFilePath(int filePath) {
+	public void setFilePath(String filePath) {
 		this.filePath = filePath;
 	}
 
@@ -74,12 +83,30 @@ public class Attachment {
 		this.boardNo = boardNo;
 	}
 
-
 	@Override
 	public String toString() {
-		return "Attachment [fileNo=" + fileNo + ", fileName=" + fileName + ", fileLevel=" + fileLevel
-				+ ", fileCreateDate=" + fileCreateDate + ", filePath=" + filePath + ", boardNo=" + boardNo + "]";
+		return "Attachment [fileNo=" + fileNo + ", fileName=" + fileName + ", fileLevel=" + fileLevel + ", filePath="
+				+ filePath + ", boardNo=" + boardNo + "]";
 	}
+
+
+
+
+	 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	 
 	 
