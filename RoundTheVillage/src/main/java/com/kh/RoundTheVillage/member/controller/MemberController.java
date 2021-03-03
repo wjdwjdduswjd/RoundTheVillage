f62c1979ae395277bf9be3f0bd1d996bfa8357d1
LeeJinSun <wldnsro1128@naver.com>
2021-03-03 12:38:46 +0900

    회원가입(주소, 유효성 검사 등 / 이메일 인증 x)

--- RoundTheVillage/src/main/java/com/kh/RoundTheVillage/member/controller/MemberController.java
+++ RoundTheVillage/src/main/java/com/kh/RoundTheVillage/member/controller/MemberController.java
@@ -1,11 +1,24 @@
 package com.kh.RoundTheVillage.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.kh.RoundTheVillage.member.model.service.MemberService;
+import com.kh.RoundTheVillage.member.model.vo.Member;
+
 
 @Controller
 @RequestMapping("/member/*")
 public class MemberController {
+	
+	@Autowired
+	private MemberService service;
+	
 	
 	//회원가입 연결
 	@RequestMapping("signUp")
@@ -13,11 +26,83 @@ public class MemberController {
 		return "member/signUp";
 	}
 	
+	//아이디 중복 체크 Controller (AJAX)
+	@RequestMapping("idDupCheck")
+	@ResponseBody
+	public int idDupCheck( @RequestParam("memberId") String memberId) { 
+		
+		// 아이디 중복 검사 서비스 호출
+		int result = service.idDupCheck(memberId);
+		
+		return result;
+	}
+	
+	//이메일 중복 체크 Controller (AJAX)
+	@RequestMapping("emailDupCheck")
+	@ResponseBody
+	public int emailDupCheck( @RequestParam("memberEmail") String memberEmail) { 
+		
+		// 아이디 중복 검사 서비스 호출
+		int result = service.idDupCheck(memberEmail);
+		
+		//System.out.println(result);
+		
+		return result;
+	}
+	
+	//닉네임 중복 체크  Controller
+	@RequestMapping("nickNameDupCheck")
+	@ResponseBody
+	public int nickNameDupCheck( @RequestParam("memberNickname") String memberNickname) {
+		int result = service.nickNameDupCheck(memberNickname);
+		
+		//System.out.println(result);
+		return result;
+	}
+	
+	
+	// 회원가입 동작
+	@RequestMapping("signUpAction")
+	public String signUpAction(@ModelAttribute Member signUpMember ,
+								RedirectAttributes ra) {
+		
+		String returnUrl = null;
+		//System.out.println(signUpMember);
+		
+		int result = service.signUp(signUpMember);
+		
+		if(result > 0) {        //추후 얼러트 창으로 바꿀 예정
+			returnUrl = "/";
+		}else {
+			returnUrl = "member/signUp";
+		}
+		
+		return "redirect:" + returnUrl ;
+	}
+	
+	
 	// 로그인화면 연결
 	@RequestMapping("login")
 	public String login() {
 		return "member/login";
 	}
+	
+	// 로그인 동작
+	@RequestMapping("loginAction")
+	public String loginAction() {
+		
+		
+		
+		
+		
+		return "";
+	}
+	
+	
+	
+	
+	
+	
 	
 	// 아이디/비밀번호 찾기 연결
 	@RequestMapping("idFind")
